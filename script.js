7884f1a7e76de4ca8b7e39607d06061a2a940597
WoraponK <worapon.klabsri@gmail.com>
2024-02-03 16:07:18 +0700

    add files

---
+++ script.js
@@ -0,0 +1,25 @@
+const cardSlider = document.querySelector('.card-slider');
+
+let clientX = null;
+let grabbing = false;
+let prevDistanceScrolled = null
+let disTanceToScroll;
+
+cardSlider.addEventListener("mousedown", (e) => {
+    clientX = e.clientX;
+    grabbing = true;
+    console.log(clientX)
+})
+
+cardSlider.addEventListener("mouseup", () => {
+    grabbing = false;
+    prevDistanceScrolled += disTanceToScroll;
+})
+
+cardSlider.addEventListener("mousemove", (e) => {
+    if (grabbing) {
+        let newClientX = e.clientX;
+        disTanceToScroll = newClientX - clientX;
+        cardSlider.style.transform = `translateX(${disTanceToScroll + prevDistanceScrolled}px)`
+    }
+})
